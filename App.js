fddcaa7619c0cec06aa6b66ae454490bc965fb75
deathRayZ <84830238+deathRayZ@users.noreply.github.com>
2024-03-16 08:01:53 +0600

    implemented bottom tab navigator
    Added bottom tab navigator. Also added credit screen.

--- App.js
+++ App.js
@@ -1,21 +1,23 @@
 // App.js
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import RecipeFeedScreen from "./screens/RecipeFeedScreen";
 import RecipeDetailScreen from "./screens/RecipeDetailScreen";
 import FavoritesScreen from "./screens/FavoritesScreen";
+import CreditScreen from "./screens/CreditScreen";
 
-const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="RecipeFeed">
-        <Stack.Screen name="RecipeFeed" component={RecipeFeedScreen} />
-        <Stack.Screen name="RecipeDetail" component={RecipeDetailScreen} />
-        <Stack.Screen name="Favorites" component={FavoritesScreen} />
-      </Stack.Navigator>
+      <Tab.Navigator>
+        <Tab.Screen name="RecipeFeed" component={RecipeFeedScreen} />
+        <Tab.Screen name="RecipeDetail" component={RecipeDetailScreen} />
+        <Tab.Screen name="Favorites" component={FavoritesScreen} />
+        <Tab.Screen name="Credits" component={CreditScreen} />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
